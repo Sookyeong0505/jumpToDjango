83d6edeedb0efa1fd0fcd331bd5cdb655987ee6b
sookyeong <sookyeong0505@gmail.com>
2024-12-19 11:17:53 +0900

    검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
@@ -6,10 +7,20 @@ from ..models import Question
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
+    # filter 함수에서 모델 속성에 접근하기 위해서는 __ (언더바 두개) 를 이용하여 하위 속성에 접근 가능
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()  # 중복 제거
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
